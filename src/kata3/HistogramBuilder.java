0137e5981c90b037e671631a794ff027864dd55e
usuario <usuario@10.22.146.192>
2013-10-18 11:28:43 +0100

    commit

---
+++ src/kata3/HistogramBuilder.java
@@ -0,0 +1,25 @@
+package kata3;
+
+public class HistogramBuilder<T> {
+    
+    public Histogram<T> histogram;
+
+    public HistogramBuilder() {
+        this.histogram= new Histogram();
+    } 
+    public void calculate (T [] data){
+        for (T item : data)
+            histogram.put(item,getCount(item)+1);
+    }
+
+    public Histogram<T> getHistogram() {
+        return histogram;
+    }
+
+    private int getCount(T item) {
+        if (histogram.containsKey(item))
+            return histogram.get(item);
+        return 0;
+    }
+    
+}
